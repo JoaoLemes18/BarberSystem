1e9e42303fd9f0921c1b3076c8f9da2437b7df9c
João Lemes <joaolemes17@hotmail.com>
2024-03-20 00:00:52 -0400

    fix: fix: vscode autoformatting texts

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,45 +1,60 @@
-import { ChangeEvent, useState } from 'react'
-import axios from 'axios';
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
 
-import Button from '../components/Button'
-import Input from '../components/Input'
-import '../styles/LoginPage.scss'
+import Button from "../components/Button";
+import Input from "../components/Input";
+import "../styles/LoginPage.scss";
 
 import { MdOutlineMailOutline } from "react-icons/md";
 import { GrSecure } from "react-icons/gr";
 
-
 interface FormState {
-  [key: string]: string
+  [key: string]: string;
 }
 
 const LoginPage = () => {
-
-  const [ form, setForm ] = useState<FormState>({})
+  const [form, setForm] = useState<FormState>({});
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setForm({...form, [name]: value})
-  }
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
   const handleSubmit = async () => {
-      await axios.post('http://localhost:3000/auth/login', form)
-  }
-
+    await axios.post("http://localhost:3000/auth/login", form);
+  };
 
   return (
-    <section className='body'>
-        <form onSubmit={handleSubmit}>
-            <h2>Bem-Vindo ao ShearSmart, <br /> Faça Login.</h2>
-            <p>Não possui uma conta? {''}
-               <a href="/register">Cadastrar</a>
-            </p> <br /> 
-            <Input onChange={handleInput} value={form.email} name='email' icon={<MdOutlineMailOutline />} type='email' placeholder='Email'/>
-            <Input onChange={handleInput} value={form.password} name='password' icon={<GrSecure />} type='password' placeholder='Senha'/>
-            <Button content='Entrar'/>
-        </form>
+    <section className="body">
+      <form onSubmit={handleSubmit}>
+        <h2>
+          Bem-Vindo ao ShearSmart, <br /> Faça Login.
+        </h2>
+        <p>
+          Não possui uma conta? {""}
+          <a href="/register">Cadastrar</a>
+        </p>{" "}
+        <br />
+        <Input
+          onChange={handleInput}
+          value={form.email}
+          name="email"
+          icon={<MdOutlineMailOutline />}
+          type="email"
+          placeholder="Email"
+        />
+        <Input
+          onChange={handleInput}
+          value={form.password}
+          name="password"
+          icon={<GrSecure />}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button content="Entrar" />
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
