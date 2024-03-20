51a09b64cbca4e8b4f615b74d30fec451d5292f4
João Lemes <joaolemes17@hotmail.com>
2024-03-19 23:37:26 -0400

    feat: structuring folders better

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const conn = require('./db/conn')
-const app = express()
+const express = require("express");
+const conn = require("./db/conn");
+const app = express();
 
-const userRouter = require('./routes/userRouter')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(userRouter);
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.use(userRouter)
-
-
-
-conn.sync().then(() => {
-    app.listen(3000, console.log('Server Running'))
-}).catch(err => console.log('Erro na conexao db', err))
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000, console.log("Server Running"));
+  })
+  .catch((err) => console.log("Erro na conexao db", err));
