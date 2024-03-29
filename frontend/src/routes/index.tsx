291e8b2ef819601ba8f28d75b231ecbf6886f774
João Lemes <joaolemes17@hotmail.com>
2024-03-29 11:44:51 -0400

    fix: creating initial route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import CrudUsers from "../pages/CrudUsers"
+import CrudUsers from "../pages/CrudUsers";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/listUsers" element={<CrudUsers />}/>
+      <Route path="/" element={<RegisterPage />} />
+      <Route path="/listUsers" element={<CrudUsers />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
